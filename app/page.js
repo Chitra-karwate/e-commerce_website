525b25bb107a974b1fb963c959ace157564be98d
devika kumari <devika21@navgurukul.org>
2023-11-28 19:10:05 +0530

    carousel is done

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar';
+import CarouselData from './Components/CarouselData';
+import "./globals.css"
+
+const page=()=>{
+  return (
+    <div>
+      <Navbar/>
+      <CarouselData/>
+    </div>
+  )
+}
+export default page;
