187a6b01bd887ce08f731e0831e6633eb4a25867
divya sable <divyasable21@navgurukul.org>
2024-01-08 18:30:14 +0530

    update footer

--- app/footer.js
+++ app/footer.js
@@ -13,73 +13,9 @@ const Footer=()=>{
                             </div> 
                             
 
-                            {/* <div className="col-2 p-5 ">
-                                <p className=" text-secondary small ">ABOUT</p>
-                                <div className=" text-white smal font-weight-bold">Contact Us</div>
-                                <div className="text-white small font-weight-bold">Careers</div>
-                                <div className="text-white small font-weight-bold">Flipkart Stories</div>
-                                <div className="text-white small">Press</div>
-                                <div className="text-white small">Flipkart wholesale</div>
-                                <div className="text-white small">Cleartrip</div>
-                                <div className="text-white small">Corporate Information</div>                                
-                            </div>
-                            <div className="col-2 p-5">
-                            <p className="text-secondary small" >HELP</p>
-                                <div className=" text-white small">Payments</div>
-                                <div className="text-white small">Shipping</div>
-                                <div className="text-white small">Cancellation & Returns</div>
-                                <div className="text-white small">FAQ</div>
-                                <div className="text-white small">Report Infringement</div>
-                            </div>
-                            <div className="col p-5 ">
-                            <p className="text-secondary small" >CONSUMER POLICY</p>
-                                <div className=" text-white small">Cancellation & Returns</div>
-                                <div className="text-white small">Terms Of Use</div>
-                                <div className="text-white small">Security</div>
-                                <div className="text-white small">Privacy</div>
-                                <div className="text-white small">Sitemap</div>
-                                <div className="text-white small">Grievance Redressal</div>
-                                <div className="text-white small">EPR Compliance</div>
-                            </div>
-                            <div className="col-2 p-5   border-left light">
-                            <p className="text-secondary small" >SOCIAL</p>
-                                <div className=" text-white small">Facebook</div>
-                                <div className="text-white small">Twitter</div>
-                                <div className="text-white small">YouTube</div>
-                                
-                            </div>
-                            
 
-                            
-                            <div className="col-2 p-5 border-left- ">
-                                <p className="text-secondary small" >Mail Us:</p>
-                                <div className=" text-white small">Flipkart Internet Private Limited</div>
-                                <div className="text-white small"> Buildings Alyssa, Begonia & </div>
-                                <div className="text-white small"> Clove Embassy Tech Village, </div>
-                                <div className="text-white small">Outer Ring Road, Devarabeesanahalli Village,</div>
-                                <div className="text-white small"> Bengaluru, 560103, </div>
-                                <div className="text-white small"> Karnataka, India</div>
-
-
-
-   
-
-                            </div>
-                            <div className="col p-5 ">
-                                <p className="text-secondary small" >Registered Office Address:</p>
-                                <div className=" text-white small">Flipkart Internet Private Limited, </div>
-                                <div className="text-white small"> Buildings Alyssa, Begonia & </div>
-                                <div className="text-white small">Clove Embassy Tech Village,</div>
-                                <div className="text-white small"> Outer Ring Road, Devarabeesanahalli Village, </div>
-                                <div className="text-white small"> Bengaluru, 560103, </div>
-                                <div className="text-white small"> Karnataka, India  </div>
-                                <div className="text-white small">  CIN : U51109KA2012PTC066107 </div>
-                            </div>
-                             */}
                         </div>
-                        {/* <hr class="m-6" style={{borderColor: "#ccc;"}}/>
-                        <div className="row bg-light"style={{height:"70;"}}></div> */}
-
+                    
                     </div>
               
         </>               
